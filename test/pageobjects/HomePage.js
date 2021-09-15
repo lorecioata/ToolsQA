43df4b86fd5aff523f4819c3d6da3b8142eb9777
Lorena Roxana Cioata <lorena.cioata@student.usv.ro>
2021-09-15 14:36:27 +0300

    message3

---
+++ test/pageobjects/HomePage.js
@@ -0,0 +1,27 @@
+class HomePage {
+  get logoImg() {
+    return $("#app img");
+  }
+  get elementsCard() {
+    return $$(".card")[0];
+  }
+  get formsCard() {
+    return $$(".card")[1];
+  }
+  get alertsCard() {
+    return $$(".card")[2];
+  }
+  get widgetsCard() {
+    return $$(".card")[3];
+  }
+  get interactionsCard() {
+    return $$(".card")[4];
+  }
+  get bookStoreCard() {
+    return $$(".card")[5];
+  }
+  get mainHeaderOfEntity() {
+    return $$(".main-header")[0];
+  }
+}
+module.exports = new HomePage();
