75c042a5d4632e262b310bf96c577ac3d4a4b81c
Lorena Roxana Cioata <lorena.cioata@student.usv.ro>
2021-09-15 17:08:19 +0300

    I have configured cross browser testing using Selenium Standalone

--- wdio.conf.js
+++ wdio.conf.js
@@ -114,7 +114,13 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ["chromedriver"],
+  services: [
+    "chromedriver",
+    [
+      "selenium-standalone",
+      { drivers: { firefox: "0.29.1", chrome: true, chromiumedge: "latest" } },
+    ],
+  ],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
