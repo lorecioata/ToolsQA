d737b6ab3b12e139b3d3681cd0d39d688d5d3841
Lorena Roxana Cioata <lorena.cioata@student.usv.ro>
2021-09-15 14:46:08 +0300

    message

--- wdio.conf.js
+++ wdio.conf.js
@@ -114,13 +114,7 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: [
-    "chromedriver",
-    [
-      "selenium-standalone",
-      { drivers: { firefox: "0.29.1", chrome: true, chromiumedge: "latest" } },
-    ],
-  ],
+  services: ["chromedriver"],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
@@ -143,15 +137,7 @@ exports.config = {
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter
 
-  reporters: [
-    "spec",
-    [
-      "allure",
-      {
-        outputDir: "allure-results",
-      },
-    ],
-  ],
+  reporters: ["spec"],
   //port: 4444,
 
   //
